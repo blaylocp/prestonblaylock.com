16437239dd57dc7839596486a2be8aad047810c0
Preston Blaylock <preston@bartonseo.com>
2016-06-29 13:37:51 -0600

    Fixed the first two gears

--- app/components/background-cog.js
+++ app/components/background-cog.js
@@ -3,24 +3,27 @@ import Ember from 'ember';
 export default Ember.Component.extend({
   cogId: null,
   direction: "forward",
+  speed: 1,
 
   didInsertElement() {
-    console.log();
     var theDirection = this.get('direction');
+    var speed = this.get('speed');
     var $cog = this.$('#' + this.get('cogId')),
         $body = this.$(document.body),
         bodyHeight = $body.height();
-        console.log($cog);
 
     this.$(window).scroll(function () {
-        if(theDirection == "forward"){
+        if(theDirection === "forward"){
           console.log("Working");
+          // $cog.css({
+          //     'transform': 'rotate(' + ($body.scrollTop() / bodyHeight * 360) + 'deg)'
+          // });
           $cog.css({
-              'transform': 'rotate(' + ($body.scrollTop() / bodyHeight * 360) + 'deg)'
+              'transform': 'rotate(' + ($body.scrollTop() * speed) + 'deg)'
           });
         } else {
           $cog.css({
-              'transform': 'rotate(-' + ($body.scrollTop() / (bodyHeight-10) * 360) + 'deg)'
+              'transform': 'rotate(-' + ($body.scrollTop() * speed) + 'deg)'
           });
         }
     });
